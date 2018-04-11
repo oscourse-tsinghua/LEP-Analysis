d526e3d5a0ad5d80501e9e721003e77e7b5485d8
LeeRainbow <2579760938@qq.com>
2018-04-11 17:46:24 +0800

    app dir

--- app/tests/runStableTests.py
+++
@@ -1,8 +0,0 @@
-import os
-import unittest
-
-test_dir = os.path.join(os.getcwd())
-discover = unittest.defaultTestLoader.discover(
-    test_dir, pattern='stabilityTests.py')
-runner = unittest.TextTestRunner()
-runner.run(discover)
