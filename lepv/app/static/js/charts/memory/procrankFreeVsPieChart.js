f74bfac7b8f43581a444199ce37afc574e599e90
LeeRainbow <2579760938@qq.com>
2018-06-21 20:08:54 +0800

    update freevstotal

--- lepv/app/static/js/charts/memory/procrankFreeVsPieChart.js
+++ lepv/app/static/js/charts/memory/procrankFreeVsPieChart.js
@@ -21,9 +21,13 @@ var ProcrankFreeVsPieChart = function(rootDivName, socket, server) {
     this.socket_response = null;
     this.chart = null;
     this.chartData = {};
-
+    this.chartData['Total Memory'] = ['Total Memory'];
+    this.chartData['Total PSS'] = ['pssTotal'];
     this.maxDataCount = 150;
     this.refreshInterval = 3;
+
+    var type = document.getElementById("type").value;
+    this.type = type;
     this.initializeChart();
     this.setupSocketIO();
 };
@@ -32,12 +36,71 @@ ProcrankFreeVsPieChart.prototype = Object.create(LepvChart.prototype);
 ProcrankFreeVsPieChart.prototype.constructor = ProcrankFreeVsPieChart;
 
 ProcrankFreeVsPieChart.prototype.initializeChart = function() {
-    
+   if (type_data_1.indexOf(this.type) != -1){
+        this.chart = c3.generate({
+        bindto: '#' + this.mainDivName,
+        data: {
+            x:'x',
+            columns: [
+                this.timeData,
+                ['Total Memory'],
+                ['Total PSS']
+            ],
+            type : this.type
+        },
+        zoom: {
+            enabled: true,
+            rescale: true
+        },
+//        subchart:{
+//            show: true,
+//        },
+        axis: {
+            x: {
+                type: 'timeseries',
+                tick: {
+                    format: '%H:%M:%S'
+                }
+            },
+            y: {
+                label: {
+                    position: "inner-middle"
+                },
+                min: 0,
+                max: undefined,
+                padding: {
+                    top:10,
+                    bottom:10
+                }
+            }
+        },
+        legend: {
+            show: true,
+            position: 'bottom',
+            inset: {
+                anchor: 'top-right',
+                x: 20,
+                y: 10,
+                step: 2
+            }
+        },
+        tooltip: {
+            format: {
+                value: function (value, ratio, id) {
+                    return value;
+                }
+            }
+        }
+        });
+    }
+   else if (type_data_2.indexOf(this.type) != -1)
+   {
    this.chart = c3.generate({
         bindto: '#' + this.mainDivName,
         data: {
-            columns: this.chartData,
-            type : 'donut'
+            columns: [['Total Memory', 0],
+                ['Total PSS', 0]],
+            type: this.type
         },
         donut: {
             title: "PSS vs. Total"
@@ -47,6 +110,7 @@ ProcrankFreeVsPieChart.prototype.initializeChart = function() {
             position: 'bottom'
         }
     });
+    }
 };
 
 ProcrankFreeVsPieChart.prototype.updateChartData = function(response) {
@@ -57,17 +121,41 @@ ProcrankFreeVsPieChart.prototype.updateChartData = function(response) {
     }
     var dataColumn = [];
 
+    if (this.chartData['Total Memory'].length > this.maxDataCount) {
+        this.timeData.splice(1, 1);
+        this.chartData['Total Memory'].splice(1, 1);
+        this.chartData['Total PSS'].splice(1, 1);
+//        this.maxValues.splice(1,1);
+    }
+
+    this.timeData.push(new Date());
+    this.chartData['Total Memory'].push(sumData['total'] - sumData['pssTotal']);
+    this.chartData['Total PSS'].push(sumData['pssTotal']);
     // to show the correct % of pss against total in donut chart, we need to to set total as (total - pss)
     dataColumn.push(["Total Memory", sumData['total'] - sumData['pssTotal']]);
     dataColumn.push(["Total PSS", sumData['pssTotal']]);
 
+    if (type_data_1.indexOf(this.type) != -1){
 
+        this.chart.load({
+            columns: [this.timeData,
+                this.chartData['Total Memory'],
+                this.chartData['Total PSS']
+                ],
+            keys: {
+                value: ['']
+            }
+        });
+    }
+    else if (type_data_2.indexOf(this.type) != -1)
+    {
     this.chart.load({
         columns: dataColumn,
         keys: {
             value: ['']
         }
     });
-    var type = document.getElementById("select").value;
-    this.chart.transform(type);
+    }
+//    var type = document.getElementById("select").value;
+//    this.chart.transform(type);
 };
