ff4311a8b4e8363f6f736ba108ce9c93f14f6ef1
LeeRainbow <2579760938@qq.com>
2018-06-21 17:22:54 +0800

    update irq

--- lepv/app/static/js/charts/cpu/cpuIrqChart.js
+++ lepv/app/static/js/charts/cpu/cpuIrqChart.js
@@ -19,6 +19,8 @@ var CpuIrqChart = function(rootDivName, socket, server) {
   this.maxDataCount = 150;
   this.timeData = ['x'];
 
+  var type = document.getElementById("type").value;
+  this.type = type;
   this.initializeChart();
   this.setupSocketIO();
 
@@ -31,15 +33,17 @@ CpuIrqChart.prototype.constructor = CpuIrqChart;
 CpuIrqChart.prototype.initializeChart = function() {
 
     var thisChart = this;
-
+    if (type_data_1.indexOf(this.type) != -1){
     this.chart = c3.generate({
         bindto: '#' + thisChart.mainDivName,
         data: {
             x: 'x',
-            columns: [thisChart.timeData]
+            columns: [thisChart.timeData],
+            type: this.type
         },
         zoom: {
-            enabled: true
+            enabled: true,
+            rescale: true
         },
         legend: {
             show: true,
@@ -79,6 +83,28 @@ CpuIrqChart.prototype.initializeChart = function() {
             }
         }
     });
+    }
+    else if (type_data_2.indexOf(this.type) != -1)
+    {
+     this.chart = c3.generate({
+        bindto: '#' + this.mainDivName,
+        data: {
+            columns: [
+
+            ],
+            type : this.type
+        },
+
+        donut:{
+            title: "irq"
+        },
+        legend: {
+            show: true,
+            position: 'right'
+        }
+    });
+    }
+
 };
 
 
@@ -94,6 +120,13 @@ CpuIrqChart.prototype.updateChartData = function(response) {
         });
 
     }
+    var chartData_irq = {};
+    $.each( data, function( coreName, statValue ) {
+         chartData_irq['CPU-' + coreName] = ['CPU-' + coreName];
+    });
+    this.chartData_1 = chartData_irq;
+    console.log(chartData_irq);
+    console.log(this.chartData_1);
     if (thisChart.timeData.length > thisChart.maxDataCount) {
         thisChart.timeData.splice(1, 1);
 
@@ -106,22 +139,34 @@ CpuIrqChart.prototype.updateChartData = function(response) {
         
     $.each( data, function( coreName, statValue ) {
         thisChart.chartData['CPU-' + coreName].push(statValue["irq"]);
+        thisChart.chartData_1['CPU-' + coreName].push(statValue['irq']);
         
     });
 
     var columnDatas = [];
+    var columnDatas_1 = [];
     columnDatas.push(thisChart.timeData);
     $.each( data, function( coreName, statValue ) {
         columnDatas.push(thisChart.chartData['CPU-' + coreName]);
+        columnDatas_1.push(thisChart.chartData_1['CPU-' + coreName]);
     });
 
+    console.log(columnDatas);
+    console.log(columnDatas_1);
+    if (type_data_1.indexOf(this.type) != -1){
     this.chart.load({
         columns: columnDatas
     });
-    
+    }
+    else if (type_data_2.indexOf(this.type) != -1)
+    {
+        this.chart.load({
+            columns: columnDatas_1
+        });
+    }
     // this.requestData();
-    var type = document.getElementById("select").value;
-    this.chart.transform(type);
+//    var type = document.getElementById("select").value;
+//    this.chart.transform(type);
 };
 
 
