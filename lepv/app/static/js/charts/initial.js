1dd6a296cd1560c446323b29053b546b9eca6162
LeeRainbow <2579760938@qq.com>
2018-06-27 12:22:11 +0800

    set the default chart

--- lepv/app/static/js/charts/initial.js
+++ lepv/app/static/js/charts/initial.js
@@ -1,21 +1,16 @@
-var InitialChart = function(rootDivName, socket, server) {
-//  LepvChart.call(this, rootDivName, socket, server);
-//
-//  this.rootDivName = rootDivName;
-//  this.socket = socket;
-//  this.serverToWatch = server;
-//  this.locateUIElements();
+var InitialChart = function() {
 
+    this.chart = c3.generate({
+//        bindto: '#' + this.mainDivName,
+        bindto: '#chart',
+        data: {
+            columns: [
+                ['data1', 30, 200, 100, 400, 150, 250],
+                ['data2', 50, 20, 10, 40, 15, 25]
+            ]
+        }
+    });
+    console.log("inti-2")
 
-  this.mainDivName = 'X-' + this.rootDivName;
-  var chart = c3.generate({
-    bindto: '#' + this.mainDivName,
-    data: {
-        columns: [
-            ['data1', 30, 200, 100, 400, 150, 250],
-            ['data2', 50, 20, 10, 40, 15, 25]
-        ]
-    }
-});
 
 };
