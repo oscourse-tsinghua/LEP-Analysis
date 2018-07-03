f7c66a4b4bf7e2e6ab7bb23abe2a260fa16e46ae
LeeRainbow <2579760938@qq.com>
2018-07-03 19:36:32 +0800

    the first select (space and chart)

--- lepv/app/static/js/charts/initial.js
+++ lepv/app/static/js/charts/initial.js
@@ -1,5 +1,4 @@
 var InitialChart = function() {
-
     this.chart = c3.generate({
 //        bindto: '#' + this.mainDivName,
         bindto: '#chart',
@@ -10,7 +9,54 @@ var InitialChart = function() {
             ]
         }
     });
-    console.log("inti-2")
-
+    console.log("inti-2");
 
 };
+
+//var InitialChart = function() {
+//    this.chart = null;
+//    init();
+//    update();
+////    this.chart = c3.generate({
+//////        bindto: '#' + this.mainDivName,
+////        bindto: '#chart',
+////        data: {
+////            columns: [
+////                ['data1', 30, 200, 100, 400, 150, 250],
+////                ['data2', 50, 20, 10, 40, 15, 25]
+////            ]
+////        }
+////    });
+//    console.log("inti-1");
+//
+//};
+//
+//var init = function(){
+//    this.chart = c3.generate({
+////        bindto: '#' + this.mainDivName,
+//        bindto: '#chart',
+//        data: {
+//            columns: [
+//                ['data1', 0],
+//                ['data2', 0]
+//            ]
+//        }
+//    });
+//    console.log("inti-2");
+//};
+//
+//var update = function(){
+//    this.chart.load({
+////        bindto: '#' + this.mainDivName,
+////        bindto: '#chart',
+//
+//        columns: [
+//            ['data1', 30, 200, 100, 400, 150, 250],
+//            ['data2', 50, 20, 10, 40, 15, 25]
+//        ],
+//        keys: {
+//            value: ['']
+//        }
+//    });
+//    console.log("inti-3");
+//};
