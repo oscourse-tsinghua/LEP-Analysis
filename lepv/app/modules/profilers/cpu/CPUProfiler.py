157027a77305094d205f4740c85a7c0042879646
LeeRainbow <2579760938@qq.com>
2018-09-03 10:03:55 +0800

    <ref> CPU softirq

--- lepv/app/modules/profilers/cpu/CPUProfiler.py
+++ lepv/app/modules/profilers/cpu/CPUProfiler.py
@@ -1010,144 +1010,161 @@ class CPUProfiler:
         return irq_data
 
     def get_softirq(self, tableinfo):
+        itemId_discovery1 = get_itemid_discovery("get_softirq[{#CPU.NUMBER},NET_TX]")
+        itemId_discovery2 = get_itemid_discovery("get_softirq[{#CPU.NUMBER},NET_RX]")
+        itemId_discovery3 = get_itemid_discovery("get_softirq[{#CPU.NUMBER},TASKLET]")
+        itemId_discovery4 = get_itemid_discovery("get_softirq[{#CPU.NUMBER},HRTIMER]")
+        core = len(itemId_discovery1)
+        print("itemid" + str(itemId_discovery1) + " " + str(itemId_discovery2) + " " + str(itemId_discovery3) + " " + str(itemId_discovery4))
+        print("core" + str(core))
+
         db = MySQLdb.connect(self.host, self.user, self.passwd, self.db)
         cursor = db.cursor()
-
+        sql = []
         if ('list5' in tableinfo):
+            ones = []
+            re = {}
+            re["time"] = 0
+            for i in range(core):
+                re['CPU-' + str(i)] = 0
+            print("re" + str(re))
             if (tableinfo['datatype'] == "NET_TX"):
-                sql = "SELECT clock,value FROM history where itemid=28301 order by itemid,clock DESC limit 1"
-                sql1 = "SELECT clock,value FROM history where itemid=28302 order by itemid,clock DESC limit 1"
-                sql2 = "SELECT clock,value FROM history where itemid=28303 order by itemid,clock DESC limit 1"
-                sql3 = "SELECT clock,value FROM history where itemid=28304 order by itemid,clock DESC limit 1"
+                for i in itemId_discovery1:
+                    sql.append("SELECT clock,value FROM history where itemid=" + str(
+                        i) + " order by itemid,clock DESC limit 1")
             elif (tableinfo['datatype'] == "NET_RX"):
-                sql = "SELECT clock,value FROM history where itemid=28306 order by itemid,clock DESC limit 1"
-                sql1 = "SELECT clock,value FROM history where itemid=28307 order by itemid,clock DESC limit 1"
-                sql2 = "SELECT clock,value FROM history where itemid=28308 order by itemid,clock DESC limit 1"
-                sql3 = "SELECT clock,value FROM history where itemid=28309 order by itemid,clock DESC limit 1"
+                for i in itemId_discovery2:
+                    sql.append("SELECT clock,value FROM history where itemid=" + str(
+                        i) + " order by itemid,clock DESC limit 1")
             elif (tableinfo['datatype'] == "TASKLET"):
-                sql = "SELECT clock,value FROM history where itemid=28311 order by itemid,clock DESC limit 1"
-                sql1 = "SELECT clock,value FROM history where itemid=28312 order by itemid,clock DESC limit 1"
-                sql2 = "SELECT clock,value FROM history where itemid=28313 order by itemid,clock DESC limit 1"
-                sql3 = "SELECT clock,value FROM history where itemid=28314 order by itemid,clock DESC limit 1"
+                for i in itemId_discovery3:
+                    sql.append("SELECT clock,value FROM history where itemid=" + str(
+                        i) + " order by itemid,clock DESC limit 1")
             elif (tableinfo['datatype'] == "HRTIMER"):
-                sql = "SELECT clock,value FROM history where itemid=28316 order by itemid,clock DESC limit 1"
-                sql1 = "SELECT clock,value FROM history where itemid=28317 order by itemid,clock DESC limit 1"
-                sql2 = "SELECT clock,value FROM history where itemid=28318 order by itemid,clock DESC limit 1"
-                sql3 = "SELECT clock,value FROM history where itemid=28319 order by itemid,clock DESC limit 1"
+                for i in itemId_discovery4:
+                    sql.append("SELECT clock,value FROM history where itemid=" + str(
+                        i) + " order by itemid,clock DESC limit 1")
 
             try:
-                sleep(60)
-                cursor.execute(sql)
-                # need to modify
-                ones = [{'time': i[0], 'CPU-0': i[1], 'CPU-1': 0, 'CPU-2': 0, 'CPU-3': 0} for i in cursor.fetchall()]
-                cursor.execute(sql1)
-                ones1 = [{'time': i[0], 'CPU-1': i[1]} for i in cursor.fetchall()]
-                cursor.execute(sql2)
-                ones2 = [{'time': i[0], 'CPU-2': i[1]} for i in cursor.fetchall()]
-                cursor.execute(sql3)
-                ones3 = [{'time': i[0], 'CPU-3': i[1]} for i in cursor.fetchall()]
-                for i in range(1):
-                    ones[i]['CPU-1'] = ones1[i]['CPU-1']
-                    ones[i]['CPU-2'] = ones2[i]['CPU-2']
-                    ones[i]['CPU-3'] = ones3[i]['CPU-3']
+                for i in range(core):
+                    cursor.execute(sql[i])
+                    result = cursor.fetchall()
+                    for row in result:
+                        re["time"] = row[0]
+                        re["CPU-"+ str(i)] = row[1]
+                        print("row"+ str(row[1]))
+                print("wwww" + str(re))
+                ones.append(re)
+
 
                 db.commit()
             except:
                 db.rollback()
         elif ('list4' in tableinfo):
+            ones = []
+            for j in range(10):
+                re = {}
+                re["time"] = 0
+                for i in range(core):
+                    re['CPU-' + str(i)] = 0
+                ones.append(re)
+            # print("re" + str(re))
+            print("ones" + str(ones))
+            temp = []
+
             if (tableinfo['datatype'] == "NET_TX"):
-                sql = "SELECT clock,value FROM history where itemid=28301 AND clock < " + tableinfo[
-                    'list4'] + "  order by itemid,clock DESC limit 10"
-                sql1 = "SELECT clock,value FROM history where itemid=28302 AND clock < " + tableinfo[
-                    'list4'] + "  order by itemid,clock DESC limit 10"
-                sql2 = "SELECT clock,value FROM history where itemid=28303 AND clock < " + tableinfo[
-                    'list4'] + "  order by itemid,clock DESC limit 10"
-                sql3 = "SELECT clock,value FROM history where itemid=28304 AND clock < " + tableinfo[
-                    'list4'] + "  order by itemid,clock DESC limit 10"
+                for i in itemId_discovery1:
+                    sql.append("SELECT clock,value FROM history where itemid=" + str(i) + " AND clock < " + tableinfo[
+                        'list4'] + " order by itemid,clock DESC limit 10")
             elif (tableinfo['datatype'] == "NET_RX"):
-                sql = "SELECT clock,value FROM history where itemid=28306 AND clock < " + tableinfo[
-                    'list4'] + "  order by itemid,clock DESC limit 10"
-                sql1 = "SELECT clock,value FROM history where itemid=28307 AND clock < " + tableinfo[
-                    'list4'] + "  order by itemid,clock DESC limit 10"
-                sql2 = "SELECT clock,value FROM history where itemid=28308 AND clock < " + tableinfo[
-                    'list4'] + "  order by itemid,clock DESC limit 10"
-                sql3 = "SELECT clock,value FROM history where itemid=28309 AND clock < " + tableinfo[
-                    'list4'] + "  order by itemid,clock DESC limit 10"
+                for i in itemId_discovery2:
+                    sql.append("SELECT clock,value FROM history where itemid=" + str(i) + " AND clock < " + tableinfo[
+                        'list4'] + " order by itemid,clock DESC limit 10")
             elif (tableinfo['datatype'] == "TASKLET"):
-                sql = "SELECT clock,value FROM history where itemid=28311 AND clock < " + tableinfo[
-                    'list4'] + "  order by itemid,clock DESC limit 10"
-                sql1 = "SELECT clock,value FROM history where itemid=28312 AND clock < " + tableinfo[
-                    'list4'] + "  order by itemid,clock DESC limit 10"
-                sql2 = "SELECT clock,value FROM history where itemid=28313 AND clock < " + tableinfo[
-                    'list4'] + "  order by itemid,clock DESC limit 10"
-                sql3 = "SELECT clock,value FROM history where itemid=28314 AND clock < " + tableinfo[
-                    'list4'] + "  order by itemid,clock DESC limit 10"
+                for i in itemId_discovery3:
+                    sql.append("SELECT clock,value FROM history where itemid=" + str(i) + " AND clock < " + tableinfo[
+                        'list4'] + " order by itemid,clock DESC limit 10")
             elif (tableinfo['datatype'] == "HRTIMER"):
-                sql = "SELECT clock,value FROM history where itemid=28316 AND clock < " + tableinfo[
-                    'list4'] + "  order by itemid,clock DESC limit 10"
-                sql1 = "SELECT clock,value FROM history where itemid=28317 AND clock < " + tableinfo[
-                    'list4'] + "  order by itemid,clock DESC limit 10"
-                sql2 = "SELECT clock,value FROM history where itemid=28318 AND clock < " + tableinfo[
-                    'list4'] + "  order by itemid,clock DESC limit 10"
-                sql3 = "SELECT clock,value FROM history where itemid=28319 AND clock < " + tableinfo[
-                    'list4'] + "  order by itemid,clock DESC limit 10"
+                for i in itemId_discovery4:
+                    sql.append("SELECT clock,value FROM history where itemid=" + str(i) + " AND clock < " + tableinfo[
+                        'list4'] + " order by itemid,clock DESC limit 10")
 
             try:
-
-                cursor.execute(sql)
-                # need to modify
-                ones = [{'time': i[0], 'CPU-0': i[1], 'CPU-1': 0, 'CPU-2': 0, 'CPU-3': 0} for i in cursor.fetchall()]
-                cursor.execute(sql1)
-                ones1 = [{'time': i[0], 'CPU-1': i[1]} for i in cursor.fetchall()]
-                cursor.execute(sql2)
-                ones2 = [{'time': i[0], 'CPU-2': i[1]} for i in cursor.fetchall()]
-                cursor.execute(sql3)
-                ones3 = [{'time': i[0], 'CPU-3': i[1]} for i in cursor.fetchall()]
-                for i in range(10):
-                    ones[i]['CPU-1'] = ones1[i]['CPU-1']
-                    ones[i]['CPU-2'] = ones2[i]['CPU-2']
-                    ones[i]['CPU-3'] = ones3[i]['CPU-3']
+                for i in range(core):
+                    cursor.execute(sql[i])
+                    result = cursor.fetchall()
+                    for row in result:
+                        temp.append(row)
+                    print("111"+ str(temp))
+                i = 0
+                while (i < 10):
+                    j = 0
+                    while (j < core):
+                        if (j == 0):
+                            ones[i]["time"] = temp[10 * j + i][0]
+                        ones[i]["CPU-" + str(j)] = temp[10 * j + i][1]
+                        j = j + 1
+                    i = i + 1
+                print("333-" + " " + str(i) + " " + str(ones))
 
                 db.commit()
             except:
                 db.rollback()
         else:
+            ones = []
+            for j in range(100):
+                re = {}
+                re["time"] = j
+                for i in range(core):
+                    re['CPU-' + str(i)] = 0
+                ones.append(re)
+            print("ones" + str(ones))
+            temp = []
+
             if (tableinfo['datatype'] == "NET_TX"):
-                sql = "SELECT clock,value FROM history where itemid=28301 order by itemid,clock DESC limit 100"
-                sql1 = "SELECT clock,value FROM history where itemid=28302 order by itemid,clock DESC limit 100"
-                sql2 = "SELECT clock,value FROM history where itemid=28303 order by itemid,clock DESC limit 100"
-                sql3 = "SELECT clock,value FROM history where itemid=28304 order by itemid,clock DESC limit 100"
+                for i in itemId_discovery1:
+                    sql.append(
+                        "SELECT clock,value FROM history where itemid=" + str(
+                            i) + " order by itemid,clock DESC limit 100")
+                print(str(sql))
+
             elif (tableinfo['datatype'] == "NET_RX"):
-                sql = "SELECT clock,value FROM history where itemid=28306 order by itemid,clock DESC limit 100"
-                sql1 = "SELECT clock,value FROM history where itemid=28307 order by itemid,clock DESC limit 100"
-                sql2 = "SELECT clock,value FROM history where itemid=28308 order by itemid,clock DESC limit 100"
-                sql3 = "SELECT clock,value FROM history where itemid=28309 order by itemid,clock DESC limit 100"
+                for i in itemId_discovery2:
+                    sql.append(
+                        "SELECT clock,value FROM history where itemid=" + str(
+                            i) + " order by itemid,clock DESC limit 100")
+                print(str(sql))
+
             elif (tableinfo['datatype'] == "TASKLET"):
-                sql = "SELECT clock,value FROM history where itemid=28311 order by itemid,clock DESC limit 100"
-                sql1 = "SELECT clock,value FROM history where itemid=28312 order by itemid,clock DESC limit 100"
-                sql2 = "SELECT clock,value FROM history where itemid=28313 order by itemid,clock DESC limit 100"
-                sql3 = "SELECT clock,value FROM history where itemid=28314 order by itemid,clock DESC limit 100"
+                for i in itemId_discovery3:
+                    sql.append(
+                        "SELECT clock,value FROM history where itemid=" + str(
+                            i) + " order by itemid,clock DESC limit 100")
+                print(str(sql))
+
             elif (tableinfo['datatype'] == "HRTIMER"):
-                sql = "SELECT clock,value FROM history where itemid=28316 order by itemid,clock DESC limit 100"
-                sql1 = "SELECT clock,value FROM history where itemid=28317 order by itemid,clock DESC limit 100"
-                sql2 = "SELECT clock,value FROM history where itemid=28318 order by itemid,clock DESC limit 100"
-                sql3 = "SELECT clock,value FROM history where itemid=28319 order by itemid,clock DESC limit 100"
+                for i in itemId_discovery4:
+                    sql.append(
+                        "SELECT clock,value FROM history where itemid=" + str(
+                            i) + " order by itemid,clock DESC limit 100")
+                print(str(sql))
 
             try:
-
-                cursor.execute(sql)
-                # need to modify
-                ones = [{'time': i[0], 'CPU-0': i[1], 'CPU-1': 0, 'CPU-2': 0, 'CPU-3': 0} for i in cursor.fetchall()]
-                cursor.execute(sql1)
-                ones1 = [{'time': i[0], 'CPU-1': i[1]} for i in cursor.fetchall()]
-                cursor.execute(sql2)
-                ones2 = [{'time': i[0], 'CPU-2': i[1]} for i in cursor.fetchall()]
-                cursor.execute(sql3)
-                ones3 = [{'time': i[0], 'CPU-3': i[1]} for i in cursor.fetchall()]
-                for i in range(100):
-                    ones[i]['CPU-1'] = ones1[i]['CPU-1']
-                    ones[i]['CPU-2'] = ones2[i]['CPU-2']
-                    ones[i]['CPU-3'] = ones3[i]['CPU-3']
+                for i in range(core):
+                    cursor.execute(sql[i])
+                    result = cursor.fetchall()
+                    for row in result:
+                        temp.append(row)
+                # print("111"+str(temp))
+                i = 0
+                while (i < 100):
+                    j = 0
+                    while (j < core):
+                        if (j == 0):
+                            ones[i]["time"] = temp[100 * j + i][0]
+                        ones[i]["CPU-" + str(j)] = temp[100 * j + i][1]
+                        j = j + 1
+                    i = i + 1
 
                 db.commit()
             except:
